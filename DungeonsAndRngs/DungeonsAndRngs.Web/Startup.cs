9adbd882263bfe10073b832e9bcc2cf7bcbe1df6
Kalinga Hulugalle <kalinga.hulugalle@gmail.com>
2016-12-18 14:49:07 +1100

    Added extra projects for web and test sites, added React and Foundation instead of bootstrap.

---
+++ DungeonsAndRngs/DungeonsAndRngs.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DungeonsAndRngs.Web.Startup))]
+namespace DungeonsAndRngs.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
